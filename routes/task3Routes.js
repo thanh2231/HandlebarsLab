b13c2697d609dd7112438a8b715a198fb092c912
LucNguyen <lucmten@gmail.com>
2022-11-29 19:24:35 +0700

    commit-hoanchinh task3

--- routes/task3Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const controller = require('../controllers/task3Controller');
-
-route.get('/',(req, res) => {
-    controller.showlist(req, res)
-})
-module.exports = route;
